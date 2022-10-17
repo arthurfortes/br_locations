0ba8ad29800e7135e0b1388b209bf9ec43ae56b4
arthurfortes <fortes.arthur@gmail.com>
2022-10-17 12:41:01 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='br_locations',
+    version='0.0.1',
+    url='https://github.com/arthurfortes/br_locations',
+    license='MIT License',
+    author='Arthur Fortes',
+    author_email='fortes.arthur@gmail.com',
+    keywords='cities states brazil',
+    description=u'Cities and States of Brazil (IBGE Info)',
+    packages=['br_locations'],
+    install_requires=[],
+)
